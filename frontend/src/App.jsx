7e240811be3ad4d4d52ea36ecd4d4164bebf1ec3
JoaoSuriano17 <joao.suriano@aluno.ifsp.edu.br>
2026-09-12 22:45:51 -0300

    Implementação de funções interligadas com o backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route, Link } from "react-router";
 import Cadastro from './Cadastro'
+import CadastroAutor from './CadastroAutor'
 import Login from './Login'
 import Home from './Home'
 import { User } from "lucide-react";
@@ -9,12 +10,127 @@ function App() {
   let a = <Link to={"/cadastro"}>Crie uma</Link>
   let b = <Link to={"/login"}>Faça login</Link>
   let c = <Link to={"/login"}><User /></Link>
+  let d = <Link to={"/cadastro"}>Crie uma</Link>
 
-  return(
+  async function loginUsuario(login, senha) {
+    try {
+      const r = await fetch("http://localhost:3000/usuarios/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          login: login,
+          senha: senha,
+        }),
+      });
+
+      const r2 = await r.json();
+
+      if (!r.ok) {
+        console.log("Erro:", r2);
+        return false;
+      }
+
+      console.log("Usuário logado:", r2);
+
+      // Guarda o usuário
+      localStorage.setItem("usuario", JSON.stringify(r2));
+
+      return true;
+
+    } catch (error) {
+      console.error("Erro ao conectar com o servidor:", error);
+      return false;
+    }
+  }
+
+  async function cadastrar(nome, login, senha, img, nacionalidade) {
+    try {
+      const r = await fetch("http://localhost:3000/usuarios", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          nome,
+          login,
+          senha,
+          img,
+          nacionalidade
+        })
+      });
+
+      const data = await r.json();
+
+      if (!r.ok) {
+        console.error("Erro:", data.msg);
+        return false;
+      }
+
+      console.log(data.msg);
+      console.log("Usuário:", data.usuario);
+
+      return true;
+
+    } catch (error) {
+      console.error("Erro ao conectar com o servidor:", error);
+      return false;
+    }
+  }
+
+  async function cadastrarAutor(nome, login, senha, img, nacionalidade, biografia, dataNascimento) {
+    try {
+      const r = await fetch("http://localhost:3000/usuarios", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          nome,
+          login,
+          senha,
+          img,
+          nacionalidade,
+          biografia,
+          data_nascimento: dataNascimento
+        })
+      });
+
+      const data = await r.json();
+
+      if (!r.ok) {
+        console.error("Erro:", data.msg);
+        return false;
+      }
+
+      console.log(data.msg);
+      console.log("Autor criado:", data.autor);
+
+      return true;
+
+    } catch (error) {
+      console.error("Erro ao conectar com o servidor:", error);
+      return false;
+    }
+  }
+
+
+  async function loginUsuarioAutor() {
+
+  }
+
+  async function loginEditora() {
+
+  }
+
+  return (
     <>
       <Routes>
-        <Route element={<Cadastro encaminhar={b}></Cadastro>} path="/cadastro"></Route>
-        <Route element={<Login encaminhar={a}></Login>} path="/login"></Route>
+        <Route element={<Cadastro encaminhar={b} cadastrar={cadastrar}></Cadastro>} path="/cadastro"></Route>
+        <Route element={<Login encaminhar={a} login={loginUsuario}></Login>} path="/login"></Route>
+        <Route element={<CadastroAutor encaminhar={d} cadastrar={cadastrarAutor}></CadastroAutor>} path="/cadastroAutor"></Route>
+
         <Route path="*" element={<Home encaminhar={c}></Home>}></Route>
       </Routes>
     </>
--- frontend/src/Cadastro.jsx
+++ frontend/src/Cadastro.jsx
@@ -1,102 +1,113 @@
-import "./cadastro.css"
+import { useState } from "react";
+import "./cadastro.css";
 
-function Cadastro(props){
+function Cadastro(props) {
+    const [nome, setNome] = useState("");
+    const [login, setLogin] = useState("");
+    const [senha, setSenha] = useState("");
+    const [img, setImg] = useState("");
+    const [nacionalidade, setNacionalidade] = useState("");
 
-    return(
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const sucesso = await props.cadastrar( nome, login, senha, img, nacionalidade );
+
+            if (sucesso) {
+                console.log("Cadastro realizado!");
+            }
+
+        } catch (error) {
+            console.error("Erro ao cadastrar:", error);
+        }
+    };
+
+    return (
         <>
             <div className="flex min-h-full rounded-md flex-col justify-center px-6 py-12 lg:px-8">
+
                 <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-                    <img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company"
-                        className="mx-auto h-10 w-auto flex centro" />
-                    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Crie sua conta
+                    <img
+                        src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
+                        alt="Your Company"
+                        className="mx-auto h-10 w-auto flex centro"
+                    />
+
+                    <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">
+                        Crie sua conta
                     </h2>
                 </div>
 
                 <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-                    <form action="#" method="POST" className="space-y-6">
+
+                    <form onSubmit={handleSubmit} className="space-y-6">
                         <div>
-                            <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">Email</label>
+                            <label htmlFor="login" className="block text-sm/6 font-medium text-gray-100">
+                                Login
+                            </label>
+
                             <div className="mt-2">
-                                <input id="email" type="email" name="email" required autoComplete="email"
-                                    className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                                <input id="login" type="text" name="login" value={login} onChange={(e) => setLogin(e.target.value)} required autoComplete="username" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"/>
                             </div>
                         </div>
 
                         <div>
-                            <label htmlFor="name" className="block text-sm/6 font-medium text-gray-100">Nome de usuário</label>
+                            <label htmlFor="name" className="block text-sm/6 font-medium text-gray-100">
+                                Nome
+                            </label>
+
                             <div className="mt-2">
-                                <input id="name" type="text" name="name" required autoComplete="name"
-                                    className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                                <input id="name" type="text" name="name" value={nome} onChange={(e) => setNome(e.target.value)} required autoComplete="name" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"/>
                             </div>
                         </div>
 
                         <div>
-                            <div className="flex items-center justify-between">
-                                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">Senha</label>
-                            </div>
+                            <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">
+                                Senha
+                            </label>
+
                             <div className="mt-2">
-                                <input id="password" type="password" name="password" required autoComplete="current-password"
-                                    className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
+                                <input id="password" type="password" name="password" value={senha} onChange={(e) => setSenha(e.target.value)} required autoComplete="new-password" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6" />
                             </div>
                         </div>
 
                         <div>
-                            <button type="submit"
-                                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">Entrar</button>
+                            <label htmlFor="img" className="block text-sm/6 font-medium text-gray-100" >
+                                Imagem
+                            </label>
+
+                            <div className="mt-2">
+                                <input id="img" type="text" name="img" value={img} onChange={(e) => setImg(e.target.value)} placeholder="URL da imagem" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"/>
+                            </div>
                         </div>
 
-                        <div className="flex flex-col text-gray-100 gap-3">
+                        <div>
+                            <label htmlFor="nacionalidade" className="block text-sm/6 font-medium text-gray-100">
+                                Nacionalidade
+                            </label>
 
-                            <div className="divider">
-                                <span className="line"></span>
-                                <span className="text-sm/6 font-medium">Ou continue com</span>
-                                <span className="line"></span>
+                            <div className="mt-2">
+                                <input id="nacionalidade"type="text" name="nacionalidade" value={nacionalidade} onChange={(e) => setNacionalidade(e.target.value)} required autoComplete="country-name" className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"/>
                             </div>
+                        </div>
 
-
-                            <div className="flex flex-col gap-3.5">
-                                <button
-                                    className="flex items-center justify-center gap-1.5 font-medium text-sm px-5 py-2 bg-gray-800 cursor-pointer rounded-lg hover:bg-gray-700">
-                                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24"
-                                        viewBox="0 0 48 48">
-                                        <path fill="#FFC107"
-                                            d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z">
-                                        </path>
-                                        <path fill="#FF3D00"
-                                            d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z">
-                                        </path>
-                                        <path fill="#4CAF50"
-                                            d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z">
-                                        </path>
-                                        <path fill="#1976D2"
-                                            d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z">
-                                        </path>
-                                    </svg>
-                                    Continue com Google
-                                </button>
-                                <button
-                                    className="flex items-center justify-center gap-1.5 font-medium text-sm px-5 py-2 bg-gray-800 cursor-pointer rounded-lg hover:bg-gray-700">
-                                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="28" height="28"
-                                        viewBox="0 0 48 48">
-                                        <path fill="#039be5" d="M24 5A19 19 0 1 0 24 43A19 19 0 1 0 24 5Z"></path>
-                                        <path fill="#fff"
-                                            d="M26.572,29.036h4.917l0.772-4.995h-5.69v-2.73c0-2.075,0.678-3.915,2.619-3.915h3.119v-4.359c-0.548-0.074-1.707-0.236-3.897-0.236c-4.573,0-7.254,2.415-7.254,7.917v3.323h-4.701v4.995h4.701v13.729C22.089,42.905,23.032,43,24,43c0.875,0,1.729-0.08,2.572-0.194V29.036z">
-                                        </path>
-                                    </svg>
-                                    Continue com Facebook
-                                </button>
-                            </div>
+                        <div>
+                            <button type="submit" className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+                                Criar conta
+                            </button>
                         </div>
 
                         <p className="mt-10 text-center text-sm/6 text-gray-400">
                             Já possui uma conta?
                             {props.encaminhar}
                         </p>
+
                     </form>
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default Cadastro
+export default Cadastro;
